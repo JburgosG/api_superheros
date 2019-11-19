e617d5e3def327d59a8d93dd498a1ac6ce5b3772
Jairo Burgos Guarin <jyburgos40@gmail.com>
2019-11-19 14:40:03 -0500

    Ranking model, migration

---
+++ database/migrations/2019_11_19_123323_create_ranking_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRankingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ranking', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hero_id')->unsigned();
+            $table->integer('like');
+            $table->integer('dont_like');
+            $table->string('ip_address');
+            $table->date('created_at');
+
+            $table->foreign('hero_id')->references('id')->on('heroes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ranking');
+    }
+}
